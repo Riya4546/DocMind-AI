98e71c185c0d903087a8389c999c877505c8cd98
Riya4546 <riyasuresh4444@gmail.com>
2026-05-20 19:36:26 +0530

    updated main.py

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Allow frontend to talk to backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
     return {
         "message": "DocMind AI Backend Running"
+    }
+
+@app.post("/process-document")
+def process_document():
+    return {
+        "vendor": "Amazon",
+        "amount": "₹2500",
+        "date": "10/05/2026"
     }
